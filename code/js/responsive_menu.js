342bfd6fbb9102141882e6730c83a491e5ea4f36
Jordi Garcia <jordigar00@gmail.com>
2022-02-16 10:45:37 +0100

    Menu responsive de galeria

---
+++ code/js/responsive_menu.js
@@ -0,0 +1,33 @@
+window.onload = main;
+
+function main() {
+    document.getElementById("menu_tancat").addEventListener("click", obreMenu);
+    document.getElementById("tancar_menu").addEventListener("click", tancaMenu);
+}
+
+//Funció per obrir el menú en telèfons
+function obreMenu() {
+    var menu_tanc = document.getElementById("menu_tancat");
+    var menu_ob = document.getElementById("menu_obert");
+
+    if(menu_ob.style.display == "") {
+        menu_tanc.style.display = "none";
+        menu_ob.style.display = "flex";
+        menu_ob.style.flexDirection = "column";
+        menu_ob.style.background = "black";
+        menu_ob.style.zIndex = 20;
+    }
+}
+
+//Funció per tancar el menú en telèfons
+function tancaMenu() {
+    var menu_tanc = document.getElementById("menu_tancat");
+    var menu_ob = document.getElementById("menu_obert");
+
+    if(menu_ob.style.display != "") {
+        menu_ob.style.display = "none";
+
+        menu_tanc.style.display = "flex";
+        menu_tanc.style.justifyContent = "center";
+    }
+}
