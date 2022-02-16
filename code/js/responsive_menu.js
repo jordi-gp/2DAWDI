e006f7c3b5bae9a8d7df82685ed319bd7c84763e
Jordi Garcia <jordigar00@gmail.com>
2022-02-16 12:00:35 +0100

    El javascript ya funciona

--- code/js/responsive_menu.js
+++ code/js/responsive_menu.js
@@ -10,13 +10,15 @@ function obreMenu() {
     var menu_tanc = document.getElementById("menu_tancat");
     var menu_ob = document.getElementById("menu_obert");
 
-    if(menu_ob.style.display == "") {
+    if(menu_ob.style.display == "" || menu_ob.style.display == "none") {
         menu_tanc.style.display = "none";
         menu_ob.style.display = "flex";
         menu_ob.style.flexDirection = "column";
         menu_ob.style.background = "black";
         menu_ob.style.zIndex = 20;
     }
+    console.log("a");
+    console.log(menu_ob.style.display)
 }
 
 //Funció per tancar el menú en telèfons
@@ -24,7 +26,7 @@ function tancaMenu() {
     var menu_tanc = document.getElementById("menu_tancat");
     var menu_ob = document.getElementById("menu_obert");
 
-    if(menu_ob.style.display != "") {
+    if(menu_ob.style.display != "none") {
         menu_ob.style.display = "none";
 
         menu_tanc.style.display = "flex";
